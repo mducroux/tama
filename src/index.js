94552e4514eaefdd588cc85b942ca8637483b665
Mathieu <mathieu.ducroux@epfl.ch>
2018-07-25 17:04:51 +0200

    Add localization support for appdrawer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
+
 import ReactDOM from "react-dom";
+import {IntlProvider, addLocaleData} from "react-intl";
+import localeEn from 'react-intl/locale-data/en';
+import localeFr from 'react-intl/locale-data/fr';
+
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import messagesFr from "./translations/fr.json";
+import messagesEn from "./translations/en.json";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+addLocaleData([...localeEn, ...localeFr]);
+
+const messages = {
+  'fr': messagesFr,
+  'en': messagesEn
+};
+
+ReactDOM.render(<IntlProvider locale='en' messages={messages.en}><App /></IntlProvider>, document.getElementById("root"));
 registerServiceWorker();
