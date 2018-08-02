594c278c5f492e152117adab481773672e4bf5e7
Mathieu <mathieu.ducroux@epfl.ch>
2018-08-02 14:03:00 +0200

    Change police

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import ReactDOM from "react-dom";
 
+import "typeface-roboto";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
