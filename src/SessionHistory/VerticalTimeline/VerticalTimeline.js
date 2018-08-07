caa339c8c707e0a60725dd6edf0b5c74ebe901db
Mathieu <mathieu.ducroux@epfl.ch>
2018-08-07 13:47:48 +0200

    Improve history scrolling

---
+++ src/SessionHistory/VerticalTimeline/VerticalTimeline.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+const VerticalTimeline = ({ animate, children, className }) => (
+  <div
+    className={classNames(className, "vertical-timeline", {
+      "vertical-timeline--animate": animate
+    })}
+  >
+    {children}
+  </div>
+);
+
+VerticalTimeline.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  className: PropTypes.string,
+  animate: PropTypes.bool
+};
+
+VerticalTimeline.defaultProps = {
+  animate: true,
+  className: ""
+};
+
+export default VerticalTimeline;
--- src/SessionHistory/index.js
+++ src/SessionHistory/index.js
@@ -4,14 +4,12 @@ import { withStyles } from "@material-ui/core/styles";
 import { FormattedMessage } from "react-intl";
 import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
 import SvgIcon from "@material-ui/core/SvgIcon";
 import { Typography } from "@material-ui/core";
 import SchoolIcon from "@material-ui/icons/School";
+
+import { VerticalTimeline, VerticalTimelineElement } from "./VerticalTimeline";
+import "./index.css";
 
 function ExampleIcon(props) {
   return (
