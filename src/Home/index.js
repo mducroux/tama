d5d1e3cd103b98c56f4e151e99d58900eccd82c1
lfaucon <lpfaucon@gmail.com>
2018-08-09 14:26:46 +0200

    Style to Home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -10,24 +10,25 @@ import { withStyles } from "@material-ui/core/styles";
 const styles = () => ({
   root: {
     display: "flex",
-    flexWrap: "wrap",
-    marginTop: "1%"
-  },
-  welcome: {
-    height: "100%"
-  },
-  studentName: {
-    textAlign: "center",
-    alignSelf: "center"
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+    alignItems: "center"
   },
   logo: {
-    height: "10%"
+    width: "auto",
+    height: "20%",
+    margin: "5%"
   },
   title: {
-    height: "3%"
+    margin: "5%"
   },
-  student: {
-    height: "60%"
+  text: {
+    width: "500px",
+    maxWidth: "100%"
+  },
+  button: {
+    marginTop: "10%"
   }
 });
 
@@ -38,23 +39,27 @@ type PropsT = {
 };
 
 const WelcomeMenu = ({ classes, isRegistered, onClickStart }: PropsT) => (
-  <div className={classes.welcome}>
-    <img src="images/logo.png" width="110" height="60" alt="logo" />
-    <Typography className={classes.title} variant="title" color="inherit">
+  <div className={classes.root}>
+    <img src="images/logo.png" alt="logo" className={classes.logo} />
+    <Typography variant="title" align="center" className={classes.title}>
       <FormattedMessage
         id="home.title"
         defaultMessage="Welcome to the game of TAMA."
       />
     </Typography>
-    <p>
-      Welcome to the game of TAMA. In this game you play the role of a teacher.
-      Everytime you play you will teach a new student by selecting activities.
-      At the end of the game your student passes a test and you earn points
-      depending on the grade obtained. Try to teach well so that your students
-      get the best grades possible!
-    </p>
+    <Typography variant="body1" align="center" className={classes.text}>
+      In this game you play the role of a teacher. Everytime you play you will
+      teach a new student by selecting activities. At the end of the game your
+      student passes a test and you earn points depending on the grade obtained.
+      Teach well so that your students get the best grades possible!
+    </Typography>
 
-    <Button variant="contained" color="primary" onClick={onClickStart}>
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={onClickStart}
+      className={classes.button}
+    >
       <FormattedMessage
         id={isRegistered ? "welcomeMenu.startPlaying" : "welcomeMenu.register"}
         defaultMessage="Start playing"
